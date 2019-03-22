fd2fa9284aa9a1269ac1ab4a22654626e6517090
JeongHo16 <cup33c@naver.com>
2019-03-22 18:21:49 +0900

    Add CollisionDetectMOCCA

---
+++ Assets/_Scripts/CollisionTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("<color=red>OnTriggerEnter: " + other.gameObject + "</color>");
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("<color=green>OnTriggerStay: " + other.gameObject + "</color>");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("<color=blue>OnTriggerExit: " + other.gameObject + "</color>");
+    }
+}
