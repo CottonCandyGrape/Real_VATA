3457d34bf33d254d8cb9e2804275fa1d5b103781
Jeongho16 <cup33c@naver.com>
2019-04-01 14:13:58 +0900

    DictionaryTest

--- Assets/_Scripts/AngleMessenger.cs
+++ Assets/_Scripts/AngleMessenger.cs
@@ -1,9 +1,22 @@
 ﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Real_VATA
 {
     public class AngleMessenger : MonoBehaviour
     {
+        //public Dictionary<>
 
+        private void Awake()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
     }
 }
