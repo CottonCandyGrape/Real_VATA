f2409599ac7545df90bac4997992f8386c47dbd7
Jeongho16 <cup33c@naver.com>
2019-04-02 14:09:35 +0900

    AngleMessenger에서 cdjoint, joint 배열 읽어오기

--- Assets/_Scripts/AngleMessenger.cs
+++ Assets/_Scripts/AngleMessenger.cs
@@ -1,22 +1,55 @@
-﻿using UnityEngine;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Real_VATA
 {
     public class AngleMessenger : MonoBehaviour
     {
-        //public Dictionary<>
+        //public Dictionary<JointName, CDJoint> cdJointDictionary = new Dictionary<JointName, CDJoint>();
+
+        private CDJointOrientationSetter cdJointOrientationSetter;
+        private JointOrientationSetter jointOrientationSetter;
+
+        private CDJoint[] cdJoints;
+        private Joint[] joints;
 
         private void Awake()
         {
+            cdJointOrientationSetter = GameObject.Find("OrientationManager_CDMOCCA").GetComponent<CDJointOrientationSetter>();
+            cdJoints = cdJointOrientationSetter.joints;
 
+            jointOrientationSetter = GameObject.Find("OrientationManager_MOCCA").GetComponent<JointOrientationSetter>();
+            joints = jointOrientationSetter.joints;
+
+            foreach (CDJoint joint in cdJoints)
+            {
+                Debug.Log("CDJoint");
+            }
+
+            foreach (Joint joint in joints)
+            {
+               Debug.Log("Joint");
+            }
         }
 
         private void Update()
         {
 
         }
+
+        void UpdateDictionary()
+        {
+
+        }
+
+        //void InitDictionary(JointName jointName, CDJoint cdJoint)
+        //{
+        //    for (int i = 0; i < (int)JointName.Length; i++)
+        //    {
+        //        cdJointDictionary.Add(jointName, cdJoint);
+        //    }
+        //}
     }
 }
--- Assets/_Scripts/CDJoint.cs
+++ Assets/_Scripts/CDJoint.cs
@@ -12,7 +12,7 @@ namespace Real_VATA
 
     public enum JointName
     {
-        shoulder_l1, shoulder_l2, elbow_l, shoulder_r1, shoulder_r2, elbow_r
+        shoulder_l1, shoulder_l2, elbow_l, shoulder_r1, shoulder_r2, elbow_r, Length
     }
 
     [Serializable]
