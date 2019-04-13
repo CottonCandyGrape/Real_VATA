68732373fcc502454e59fc78adf194cf47b9fbf6
Jeongho16 <cup33c@naver.com>
2019-04-13 14:11:42 +0900

    Test FaceTracking

--- Assets/Facetracking Starter Kit/Scripts/Kinect/FaceTrackingExample.cs
+++ Assets/Facetracking Starter Kit/Scripts/Kinect/FaceTrackingExample.cs
@@ -8,9 +8,9 @@ public class FaceTrackingExample : MonoBehaviour
     public float SmoothTime = 0.1f;
     public KinectBinder Kinect;
     public Transform Model;
-    public PoseAnimator ModelAnimator;
+    //public PoseAnimator ModelAnimator;
 
-    public GameObject Flames;
+    //public GameObject Flames;
 
     private Vector3 _position;
     private Vector3 _rotation;
@@ -18,8 +18,8 @@ public class FaceTrackingExample : MonoBehaviour
     private Vector3 _currentPosVelocity;
     private Vector3 _currentRotVelocity;
 
-    private AnimationUnits _animUnits, _targetAnimUnits;
-    private AnimationUnits _currentAuVelocity;
+    //private AnimationUnits _animUnits, _targetAnimUnits;
+    //private AnimationUnits _currentAuVelocity;
 
     private bool _isInitialized;
     private bool _hasNewData;
@@ -63,27 +63,27 @@ public class FaceTrackingExample : MonoBehaviour
 
         // We amplify the position to exagerate the head movements.
         _position *= 10;
-        SetCurrentAUs(animUnits);
+        //SetCurrentAUs(animUnits);
     }
 
 
-    private void SetCurrentAUs(AnimationUnits animUnits)
-    {
-        const float weight = 0.8f;
-        for (int i = 0; i < 6; i++)
-        {
-            _accAu[i] = animUnits[i] * weight + _accAu[i] * (1 - weight);
-        }
+    //private void SetCurrentAUs(AnimationUnits animUnits)
+    //{
+    //    const float weight = 0.8f;
+    //    for (int i = 0; i < 6; i++)
+    //    {
+    //        _accAu[i] = animUnits[i] * weight + _accAu[i] * (1 - weight);
+    //    }
 
-        animUnits = _accAu;
+    //    animUnits = _accAu;
 
-        _targetAnimUnits.LipRaiser = MapLipRaiserValue(animUnits.LipRaiser);
-        _targetAnimUnits.JawLowerer = MapJawLowererValue(animUnits.JawLowerer);
-        _targetAnimUnits.LipStretcher = MapLipStretcherValue(animUnits.LipStretcher);
-        _targetAnimUnits.BrowLowerer = MapBrowLowererValue(animUnits);
-        _targetAnimUnits.LipCornerDepressor = MapLipCornerDepressorValue(animUnits.LipCornerDepressor);
-        _targetAnimUnits.OuterBrowRaiser = MapOuterBrowRaiserValue(animUnits.OuterBrowRaiser);
-    }
+    //    _targetAnimUnits.LipRaiser = MapLipRaiserValue(animUnits.LipRaiser);
+    //    _targetAnimUnits.JawLowerer = MapJawLowererValue(animUnits.JawLowerer);
+    //    _targetAnimUnits.LipStretcher = MapLipStretcherValue(animUnits.LipStretcher);
+    //    _targetAnimUnits.BrowLowerer = MapBrowLowererValue(animUnits);
+    //    _targetAnimUnits.LipCornerDepressor = MapLipCornerDepressorValue(animUnits.LipCornerDepressor);
+    //    _targetAnimUnits.OuterBrowRaiser = MapOuterBrowRaiserValue(animUnits.OuterBrowRaiser);
+    //}
 
 
     #region Au Range Calibration
@@ -148,7 +148,7 @@ public class FaceTrackingExample : MonoBehaviour
             float timeSinceLastFrame = Time.time - _timeOfLastFrame;
             if (timeSinceLastFrame > TimeToReturnToDefault + FaceTrackingTimeout)
             {
-                ManipulateMaskAutomatically();
+                //ManipulateMaskAutomatically();
             }
             else if (timeSinceLastFrame > FaceTrackingTimeout)
             {
@@ -157,9 +157,9 @@ public class FaceTrackingExample : MonoBehaviour
         }
 
         UpdateTransform();
-        UpdateAUs();
+        //UpdateAUs();
 
-        CheckForSpecialPoses();
+        //CheckForSpecialPoses();
     }
 
 
@@ -182,19 +182,19 @@ public class FaceTrackingExample : MonoBehaviour
 
 
     // Perform random faces automatically to fill the gaps whenever we do not have any input data from kinect.
-    private void ManipulateMaskAutomatically()
-    {
-        CurrentMode = TrackingMode.ComputerControlled;
-        _waitTimer -= Time.deltaTime;
-        if (_waitTimer > 0)
-            return;
+    //private void ManipulateMaskAutomatically()
+    //{
+    //    CurrentMode = TrackingMode.ComputerControlled;
+    //    _waitTimer -= Time.deltaTime;
+    //    if (_waitTimer > 0)
+    //        return;
 
-        _waitTimer = 3f;
+    //    _waitTimer = 3f;
 
-        _rotation = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), Random.Range(-10f, 10f));
-        _targetAnimUnits = new AnimationUnits(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f),
-                                          Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-    }
+    //    _rotation = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), Random.Range(-10f, 10f));
+    //    _targetAnimUnits = new AnimationUnits(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f),
+    //                                      Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+    //}
 
 
     private void ReturnMaskToNeutralState()
@@ -214,7 +214,7 @@ public class FaceTrackingExample : MonoBehaviour
     {
         _isInitialized = false;
         _gravityStartTime = Time.time;
-        _startGravityAu = _animUnits;
+        //_startGravityAu = _animUnits;
         _startGravityPos = _position;
         _startGravityRot = _rotation;
     }
@@ -229,7 +229,7 @@ public class FaceTrackingExample : MonoBehaviour
         var animUnits = new AnimationUnits();
         animUnits.Au012 = Vector3.Lerp(_startGravityAu.Au012, Vector3.zero, time);
         animUnits.Au345 = Vector3.Lerp(_startGravityAu.Au345, Vector3.zero, time);
-        SetCurrentAUs(animUnits);
+        //SetCurrentAUs(animUnits);
     }
 
 
@@ -242,96 +242,93 @@ public class FaceTrackingExample : MonoBehaviour
     }
 
 
-    private void UpdateAUs()
-    {
-        // Smooth the animation units as the data received directly by the kinect is noisy.
-        _animUnits.Au012 = Vector3.SmoothDamp(_animUnits.Au012, _targetAnimUnits.Au012, ref _currentAuVelocity.Au012, SmoothTime);
-        _animUnits.Au345 = Vector3.SmoothDamp(_animUnits.Au345, _targetAnimUnits.Au345, ref _currentAuVelocity.Au345, SmoothTime);
+    //private void UpdateAUs()
+    //{
+    //    // Smooth the animation units as the data received directly by the kinect is noisy.
+    //    _animUnits.Au012 = Vector3.SmoothDamp(_animUnits.Au012, _targetAnimUnits.Au012, ref _currentAuVelocity.Au012, SmoothTime);
+    //    _animUnits.Au345 = Vector3.SmoothDamp(_animUnits.Au345, _targetAnimUnits.Au345, ref _currentAuVelocity.Au345, SmoothTime);
 
-        UpdateLipRaiser(_animUnits.LipRaiser);
-        UpdateJawLowerer(_animUnits.JawLowerer);
-        UpdateLipStretcher(_animUnits.LipStretcher);
-        UpdateBrowLowerer(_animUnits.BrowLowerer);
-        UpdateLipCornerDepressor(_animUnits.LipCornerDepressor);
-        UpdateOuterBrowRaiser(_animUnits.OuterBrowRaiser);
-    }
+    //    UpdateLipRaiser(_animUnits.LipRaiser);
+    //    UpdateJawLowerer(_animUnits.JawLowerer);
+    //    UpdateLipStretcher(_animUnits.LipStretcher);
+    //    UpdateBrowLowerer(_animUnits.BrowLowerer);
+    //    UpdateLipCornerDepressor(_animUnits.LipCornerDepressor);
+    //    UpdateOuterBrowRaiser(_animUnits.OuterBrowRaiser);
+    //}
 
-    #region Specific AU Updates
-    /**
-     * Note on animating the mask:
-     * In this example we use a pose animation technology to animate the mask.
-     * You could just as easily use unity animation system and control the animation timeline yourself,
-     * or control the animations by direct manipulation of the bones. Whatever suits you best.
-     * 
-     * In general, we have one or two specific pose (animation) per Animation Unit (AU).
-     * This setup was simply motived by its ease of use.
-     * 
-     * Finally you will notice that sometimes we do not use the full range or that we use some magic numbers.
-     * These were hand tweaked so that we could better exagerate certain expressions. In that case we decided
-     * to go benefit the user experience instead of plain data accuracy.
-     */
-    private void UpdateLipRaiser(float coef)
-    {
-        _morphCoefs[0] = coef;
-        ModelAnimator.SetWeight(0, coef);
-    }
+    //#region Specific AU Updates
+    ///**
+    // * Note on animating the mask:
+    // * In this example we use a pose animation technology to animate the mask.
+    // * You could just as easily use unity animation system and control the animation timeline yourself,
+    // * or control the animations by direct manipulation of the bones. Whatever suits you best.
+    // * 
+    // * In general, we have one or two specific pose (animation) per Animation Unit (AU).
+    // * This setup was simply motived by its ease of use.
+    // * 
+    // * Finally you will notice that sometimes we do not use the full range or that we use some magic numbers.
+    // * These were hand tweaked so that we could better exagerate certain expressions. In that case we decided
+    // * to go benefit the user experience instead of plain data accuracy.
+    // */
+    //private void UpdateLipRaiser(float coef)
+    //{
+    //    _morphCoefs[0] = coef;
+    //    ModelAnimator.SetWeight(0, coef);
+    //}
 
-    private void UpdateJawLowerer(float coef)
-    {
-        // The jaw lowerer animation unit has no negative range.
-        _morphCoefs[1] = Mathf.Max(0, coef);
-        ModelAnimator.SetWeight(1, Mathf.Max(0, coef));
-    }
+    //private void UpdateJawLowerer(float coef)
+    //{
+    //    // The jaw lowerer animation unit has no negative range.
+    //    _morphCoefs[1] = Mathf.Max(0, coef);
+    //    ModelAnimator.SetWeight(1, Mathf.Max(0, coef));
+    //}
 
-    private void UpdateLipStretcher(float coef)
-    {
-        // The lip stretcher animation has 2 animations simply because it was easier to design that way.
-        // One represents the Animation Unit range [-1, 0] and the other is for [0, 1].
-        _morphCoefs[2] = Mathf.Clamp(-1.5f * coef, 0, 1.5f);
-        _morphCoefs[3] = Mathf.Clamp(coef, -0.7f, 1);
-        ModelAnimator.SetWeight(2, Mathf.Clamp(-1.5f * coef, 0, 1.5f));
-        ModelAnimator.SetWeight(3, Mathf.Clamp(coef, -0.7f, 1));
-    }
+    //private void UpdateLipStretcher(float coef)
+    //{
+    //    // The lip stretcher animation has 2 animations simply because it was easier to design that way.
+    //    // One represents the Animation Unit range [-1, 0] and the other is for [0, 1].
+    //    _morphCoefs[2] = Mathf.Clamp(-1.5f * coef, 0, 1.5f);
+    //    _morphCoefs[3] = Mathf.Clamp(coef, -0.7f, 1);
+    //    ModelAnimator.SetWeight(2, Mathf.Clamp(-1.5f * coef, 0, 1.5f));
+    //    ModelAnimator.SetWeight(3, Mathf.Clamp(coef, -0.7f, 1));
+    //}
 
-    private void UpdateBrowLowerer(float coef)
-    {
-        _morphCoefs[4] = coef;
-        ModelAnimator.SetWeight(4, coef);
-    }
+    //private void UpdateBrowLowerer(float coef)
+    //{
+    //    _morphCoefs[4] = coef;
+    //    ModelAnimator.SetWeight(4, coef);
+    //}
 
-    private void UpdateLipCornerDepressor(float coef)
-    {
-        _morphCoefs[5] = Mathf.Clamp(coef, -0.15f, 1);
-        ModelAnimator.SetWeight(5, Mathf.Clamp(coef, -0.15f, 1));
-    }
+    //private void UpdateLipCornerDepressor(float coef)
+    //{
+    //    _morphCoefs[5] = Mathf.Clamp(coef, -0.15f, 1);
+    //    ModelAnimator.SetWeight(5, Mathf.Clamp(coef, -0.15f, 1));
+    //}
 
-    private void UpdateOuterBrowRaiser(float coef)
-    {
-        _morphCoefs[6] = coef;
-        ModelAnimator.SetWeight(6, coef);
-    }
-    #endregion
-
-
-    private void CheckForSpecialPoses()
-    {
-        if (IsHappy())
-        {
-            if (!Flames.activeSelf)
-                Flames.SetActive(true);
-        }
-        else
-        {
-            if (Flames.activeSelf)
-                Flames.SetActive(false);
-        }
-    }
-
-    private bool IsHappy()
-    {
-        return _morphCoefs[1] > 0.6f && _morphCoefs[6] > 0.35f;
-    }
+    //private void UpdateOuterBrowRaiser(float coef)
+    //{
+    //    _morphCoefs[6] = coef;
+    //    ModelAnimator.SetWeight(6, coef);
+    //}
+    //#endregion
 
 
+    //private void CheckForSpecialPoses()
+    //{
+    //    if (IsHappy())
+    //    {
+    //        if (!Flames.activeSelf)
+    //            Flames.SetActive(true);
+    //    }
+    //    else
+    //    {
+    //        if (Flames.activeSelf)
+    //            Flames.SetActive(false);
+    //    }
+    //}
 
+    //private bool IsHappy()
+    //{
+    //    return _morphCoefs[1] > 0.6f && _morphCoefs[6] > 0.35f;
+    //}
 }
